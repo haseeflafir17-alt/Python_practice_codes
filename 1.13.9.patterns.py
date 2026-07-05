0b2e7b493cc4257400949ba68a63e576d483048f
haseeflafir17-alt <haseeflafir17@gmail.com>
2026-07-05 23:58:18 +0530

    input rows and columns

--- 1.13.9.patterns.py
+++ 1.13.9.patterns.py
@@ -1,8 +1,9 @@
 row = int(input("Enter the rows: "))
+column = int(input("Enter the columns: "))
 
 for x in range(row):
-    for y in range(row):
-        if x == 0 or x== row-1 or y == 0 or y == row -1:
+    for y in range(column):
+        if x == 0 or x== row-1 or y == 0 or y == column -1:
             print("* ", end="")
         else:
             print("  ",end="")
