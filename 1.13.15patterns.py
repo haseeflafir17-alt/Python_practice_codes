d7ec1c49af278f148b25eb03ad9c5c5eee6ea7c3
haseeflafir17-alt <haseeflafir17@gmail.com>
2026-07-06 11:09:08 +0530

    number pattern5

---
+++ 1.13.15patterns.py
@@ -0,0 +1,8 @@
+row = int(input("Enter the number: "))
+
+for x in range(1,row+1):
+    for y in range(1,x):
+        print("  ",end="")
+    for z in range(row,x-1,-1):
+        print(f"{z}",end=" ")
+    print()
